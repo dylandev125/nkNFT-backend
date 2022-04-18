281c3b150567ddfc2dff60785a57cca2e9219269
dylandev125 <pauleta0126@gmail.com>
2022-04-18 10:32:57 +0300

    fix signup problem, api fix

--- src/routes/merkletree/merkletree.router.js
+++ src/routes/merkletree/merkletree.router.js
@@ -1,21 +1,51 @@
 const express = require('express');
+const fs = require('fs');
+const auth = require('../../middleware/auth');
 
 const {
     verifyWhitelist,
+    addWhiteList,
+    removeWhiteList
 } = require('./merkletree.controller');
 
 const merkleRouter = express.Router();
 
 merkleRouter.get('/:address', (req, res) => {
 
-    try{
+    try {
         let value = verifyWhitelist(req.params.address);
         res.status(200).send({
             ...value,
             success: true,
         });
     }
-    catch(error) {
+    catch (error) {
+        console.log(error);
+    }
+});
+
+merkleRouter.post('/addlist', auth, (req, res) => {
+    try {
+        const { whitelist } = req.body;
+        if(!whitelist) {
+            res.status(200).send("no list");
+            return;
+        }
+        addWhiteList(whitelist);
+        res.status(200).send(req.user);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+merkleRouter.post('/removelist', auth, (req, res) => {
+    try {
+        const { whitelist } = req.body;
+        removeWhiteList(whitelist);
+        res.status(200).send(req.user);
+    }
+    catch (error) {
         console.log(error);
     }
 });
